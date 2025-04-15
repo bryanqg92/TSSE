eac926025dc0f3ef5577c6ea193fa721d7043f9c
Leandro Quiroga <bryanqg92@gmail.com>
2025-04-14 20:45:21 -0500

    fixing run for uses on pre commit validation

--- TP4/test/test_leds.c
+++ TP4/test/test_leds.c
@@ -75,14 +75,14 @@ void test_prender_y_apagar_multiples_leds (void) {
     LedsTurnOffAll();
     LedsTurnOnSingle(16);
     LedsTurnOnSingle(14);
-    LedsTurnOnSingle(11);
-    LedsTurnOnSingle(9);
+    LedsTurnOnSingle(12);
+    LedsTurnOnSingle(10);
     LedsTurnOnSingle(8);
     LedsTurnOnSingle(6);
-    LedsTurnOnSingle(3);
-    LedsTurnOnSingle(1);
+    LedsTurnOnSingle(4);
+    LedsTurnOnSingle(2);
 
-    TEST_ASSERT_EQUAL_HEX16(0xA5A5, leds_virtuales);
+    TEST_ASSERT_EQUAL_HEX16(0xAAAA, leds_virtuales);
 }
 
 //@test Prender todos los LEDs de una vez.
